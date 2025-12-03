07114898247d690925f745bdc305288fb6bc09a0
Zaid Siddique <zaidyousuf.siddique3@gmail.com>
2025-12-03 20:16:17 +0300

    Fix: Add proper routing with LanguageSelectionPage as landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { LanguageProvider } from './contexts/LanguageContext'
+import { LanguageSelectionPage } from './pages/LanguageSelectionPage'
+import { KnowledgePage } from './pages/KnowledgePage'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  \u003cReact.StrictMode\u003e
+  \u003cLanguageProvider\u003e
+  \u003cBrowserRouter\u003e
+  \u003cRoutes\u003e
+  \u003cRoute path = "/" element = { \u003cLanguageSelectionPage /\u003e} /\u003e
+  \u003cRoute path = "/home" element = { \u003cApp /\u003e} /\u003e
+  \u003cRoute path = "/knowledge" element = { \u003cKnowledgePage /\u003e} /\u003e
+  \u003cRoute path = "*" element = { \u003cNavigate to="/" replace /\u003e} /\u003e
+  \u003c / Routes\u003e
+  \u003c / BrowserRouter\u003e
+  \u003c / LanguageProvider\u003e
+  \u003c / React.StrictMode\u003e
 )
